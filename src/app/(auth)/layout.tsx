29be6e1d101d5c2e3fa1a34138548015a3d3ad1d
Samir Chaudhary <samirchaudhary.cs23@bmsce.ac.in>
2026-02-25 17:44:33 +0000

    I MEAN THE SURROUNDING PACKGROUND

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({
   return (
     <main className="relative min-h-svh flex items-center justify-center p-4">
         <Image
-            src="https://images.unsplash.com/photo-1677442135729-a882b4355594?q=80&w=1932&auto=format&fit=crop"
+            src="https://images.unsplash.com/photo-1690174783344-3d943a85b939?q=80&w=1932&auto=format&fit=crop"
             alt="Abstract robotics design background"
             fill
             className="object-cover -z-10 filter blur-md brightness-[.4]"
