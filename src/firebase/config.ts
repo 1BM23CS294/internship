842f255e00805fbdc44484574d2c6831c7f88b8d
Samir Chaudhary <samirchaudhary.cs23@bmsce.ac.in>
2026-02-25 17:28:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3425626014-bf5d2",
+  "appId": "1:575624716040:web:e360ed91244116e6bcd6bd",
+  "apiKey": "AIzaSyC1fAw-PR7eJUCzUkoKxAeA6hYOItpSGFU",
+  "authDomain": "studio-3425626014-bf5d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "575624716040"
+};
